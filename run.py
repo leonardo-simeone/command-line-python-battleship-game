01d6d3755393cb1d37d5176806536b17b0e3fd5d
Leonardo Simeone <leo_sime@yahoo.com>
2023-01-12 14:24:44 +0000

    Create method to print user board

--- run.py
+++ run.py
@@ -9,3 +9,12 @@ class GameBoard:
         letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4,
                               "F": 5, "G": 6, "H": 7}
         return letters_to_numbers
+
+    # Method used to print the user's board
+    def print_board(self):
+        print("  A B C D E F G H")
+        print("  +-+-+-+-+-+-+-+")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
