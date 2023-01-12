e257c334788cb22874cb18687dcba21144882c7b
Leonardo Simeone <leo_sime@yahoo.com>
2023-01-12 14:20:54 +0000

    Create method to get user input letter from dict

--- run.py
+++ run.py
@@ -3,3 +3,9 @@ class GameBoard:
 
     def __init__(self, board):
         self.board = board
+
+    # Method used to return the user's input key utilizing a dictionary
+    def convert_letters_to_numbers():
+        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4,
+                              "F": 5, "G": 6, "H": 7}
+        return letters_to_numbers
