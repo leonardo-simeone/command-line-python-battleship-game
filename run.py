320226d0f665310093c0dc121ca16bf9ae5d7a31
Leonardo Simeone <leo_sime@yahoo.com>
2023-01-12 16:13:50 +0000

    Add docstrings to gameboard class and its methods

--- run.py
+++ run.py
@@ -3,24 +3,29 @@ import random # Import random module
 
 class GameBoard:
     """
-Create an object of an entity board. This class
-will create 2 objects, the user's board and the computer's
-board. It also has methods that will allow to print the user's board
-while keeping the computer's hidden and return the column value to the
-user's input key by using the letters_to_numbers dictionary.
-"""
+    Create an object of an entity board. This class
+    will create 2 objects, the user's board and the computer's
+    board. It also has methods that will allow to print the user's board
+    while keeping the computer's hidden and return the column value to the
+    user's input key by using the letters_to_numbers dictionary.
+    """
 
     def __init__(self, board):
         self.board = board
 
-    # Method used to return the user's input key utilizing a dictionary
     def convert_letters_to_numbers():
+        """
+        Method used to return the user's
+        input key utilizing a dictionary
+        """
         letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4,
                               "F": 5, "G": 6, "H": 7}
         return letters_to_numbers
 
-    # Method used to print the user's board
     def print_board(self):
+        """ 
+        Method used to print the user's board
+        """
         print("  A B C D E F G H")
         print("  +-+-+-+-+-+-+-+")
         row_number = 1
